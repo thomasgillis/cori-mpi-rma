42189a59130eb7f22e045a8e11ba46c3dd329eee
Thomas Gillis <thoma.gillis@gmail.com>
2021-10-05 09:22:09 -0700

    improve the testcase to involve multiple Get calls + datatypes + only communication between rank 3 and rank 35

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <cstring>
 #include <limits>
 
+#define N 40
+#define NS 10
 
 int main(int argc, char** argv){
     MPI_Init(&argc,&argv);
@@ -18,52 +20,109 @@ int main(int argc, char** argv){
     int len;
     MPI_Get_processor_name( name, &len );
 
-    printf("[rank %d] the world is %d ranks wide and my name is %s\n",rank,comm_size,name);
+    if(rank == 3 || rank == 35){
+        printf("[rank %d] the world is %d ranks wide and my name is %s\n",rank,comm_size,name); fflush(stdout);
+    }
 
-    // allocate the array
-    int* array = (int*) malloc(sizeof(int)*1);
-    array[0] = rank;
-
+    // allocate the array of size [10x10x10]
+    int size = N*N*N;
+    int* array = (int*) malloc(sizeof(int)*size);
+    for(int i=0; i<size; ++i){
+        array[i] = rank;
+    }
 
     MPI_Info info;
     MPI_Info_create(&info);
     MPI_Win  window = MPI_WIN_NULL;
-    MPI_Win_create(array,sizeof(int), sizeof(int), info, MPI_COMM_WORLD, &window);
+    MPI_Win_create(array,size*sizeof(int), sizeof(int), info, MPI_COMM_WORLD, &window);
     MPI_Info_free(&info);
 
     // create the group
+    //int next_rank = (comm_size + rank + 1)%comm_size;
+    //int prev_rank = (comm_size + rank - 1)%comm_size;
+    int next_rank = 0 + (rank == 3) * 35 + (rank == 35) * 3;
+    int prev_rank = 0 + (rank == 3) * 35 + (rank == 35) * 3;
+    if(rank == 3 || rank == 35){
+        printf("[rank %d] I am between rank %d and rank %d\n",rank,prev_rank,next_rank); fflush(stdout);
+    }
     MPI_Group prev_group, next_group, global_group;
     MPI_Comm_group(MPI_COMM_WORLD, &global_group);
-    int next_rank = (comm_size + rank + 1)%comm_size;
-    int prev_rank = (comm_size + rank - 1)%comm_size;
-    printf("[rank %d] I am between rank %d and rank %d\n",rank,prev_rank,next_rank);
-    
     MPI_Group_incl(global_group,1,&prev_rank,&prev_group);
     MPI_Group_incl(global_group,1,&next_rank,&next_group);
     MPI_Group_free(&global_group);
 
 
+    // get the datatype to get the rank in an array of size [2,2,2]
+    MPI_Datatype type1, type2;
+    MPI_Type_create_hvector(NS,NS,N*sizeof(int),MPI_INT,&type1);
+    MPI_Type_create_hvector(NS,1,N*N*sizeof(int),type1,&type2);
+    MPI_Type_commit(&type2);
+    MPI_Type_free(&type1);
 
     // start
     MPI_Win_post(prev_group,0,window);
     MPI_Win_start(next_group,0,window);
     
-    int other;
-    MPI_Get(&other,1,MPI_INT,next_rank,0,1,MPI_INT,window);
+    int* other = (int*) malloc(sizeof(int)*size);
+    
+    int offset1 = 0 + N * ( 0 + N * 0);
+    int offset2 = 10 + N * ( 10 + N * 10);
+
+    if(rank == 3 || rank == 35){
+        printf("starting the MPI_Gets\n"); fflush(stdout);
+    }
+    if(rank == 35 || rank == 3){
+        MPI_Get(other+offset1,1,type2,next_rank,offset1,1,type2,window);
+        
+        MPI_Get(other+offset2,1,type2,next_rank,offset2,1,type2,window);
+
+        //int offset3 = 5 + N * ( 2 + N * 7);
+        //MPI_Get(other+offset,1,type2,next_rank,offset,1,type2,window);
+
+        //int offset4 = 4 + N * ( 5 + N * 1);
+        //MPI_Get(other+offset,1,type2,next_rank,offset,1,type2,window);
+
+        MPI_Type_free(&type2);
+    }
+    if(rank == 3 || rank == 35){
+        printf("starting the MPI_Win_complete\n"); fflush(stdout);
+    }
 
     MPI_Win_complete(window);
+    if(rank == 3 || rank == 35){
+        printf("starting the MPI_Win_wait\n"); fflush(stdout);
+    }
     MPI_Win_wait(window);
 
 
-    printf("I am rank %d and the next one is %d (answer = %d)\n",rank,other,next_rank);
+    if(rank == 35 || rank == 3){
+        printf("I am rank %d and the next one is %d (answer = %d)\n",rank,other[offset1],next_rank); fflush(stdout);
+        printf("I am rank %d and the next one is %d (answer = %d)\n",rank,other[offset2],next_rank); fflush(stdout);
+    }
 
 
     // free
+    if(rank == 3 || rank == 35){
+        printf("free the groups\n"); fflush(stdout);
+    }
     MPI_Group_free(&next_group);
     MPI_Group_free(&prev_group);
-    MPI_Win_free(&window);
-    free(array);
 
+    if(rank == 3 || rank == 35){
+        printf("free the window\n"); fflush(stdout);
+    }
+    MPI_Win_free(&window);
+    
+    if(rank == 3 || rank == 35){
+        printf("free the arrays\n"); fflush(stdout);
+    }
+    free(array);
+    free(other);
+
+
+    if(rank == 3 || rank == 35){
+        printf("Finalize\n"); fflush(stdout);
+    }
 
 
     MPI_Finalize();
