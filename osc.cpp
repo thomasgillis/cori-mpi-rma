05fe19cda88c66eb97d274f631aa2b4064215bcc
Thomas Gillis <thoma.gillis@gmail.com>
2023-01-19 15:03:53 -0600

    add a contiguous memory option + control of N and NS from the defines

--- osc.cpp
+++ osc.cpp
@@ -11,16 +11,32 @@
 #define COMM_RMA_ACTV 1
 #define COMM_RMA_FENC 2
 
+#define MEM_DATATYPE 0
+#define MEM_CONTIG 1
+
 #ifdef COMM_MODE
 #define M_COMM COMM_MODE
 #else
 #define M_COMM COMM_SENDRECV
 #endif
+#ifdef MEM
+#define M_MEM MEM
+#else
+#define M_MEM MEM_CONTIG
+#endif
 
 // the total communication size will be N^3 and split-up in NS^3 chunks, each chunk will be sent out 
 // independently
-#define N 100 // N is the 1D size of the 3D array
-#define NS 5 // NS is the size of the subarray (exchanged as one block)
+#ifdef N
+#define M_N N
+#else
+#define M_N 100
+#endif
+#ifdef NS
+#define M_NS NS
+#else
+#define M_NS 5 // NS is the size of the subarray (exchanged as one block)
+#endif
 #define IMAX 100
 
 static int get_next_rank(const int rank, const int comm_size) {
@@ -47,7 +63,7 @@ int main(int argc, char** argv){
 
     //--------------------------------------------------------------------------
     // allocate the array of size [N x N x N]
-    int  size  = N * N * N;
+    int  size  = M_N * M_N * M_N;
     int* array = (int*)malloc(sizeof(int) * size);
     int* other = (int*)malloc(sizeof(int) * size);
     for (int i = 0; i < size; ++i) {
@@ -80,7 +96,7 @@ int main(int argc, char** argv){
     MPI_Group_free(&global_group);
 
 #if (M_COMM == COMM_SENDRECV)
-        const int nreq = (N / NS) * (N / NS) * (N / NS);
+        const int nreq = (M_N / M_NS) * (M_N / M_NS) * (M_N / M_NS);
         MPI_Request *sreq, *rreq;
         sreq = (MPI_Request *)malloc(sizeof(MPI_Request) * nreq);
         rreq = (MPI_Request *)malloc(sizeof(MPI_Request) * nreq);
@@ -99,18 +115,26 @@ int main(int argc, char** argv){
 
         // for each submatrix of size NSxNSxNS
         int ireq = 0;
-        for (int i2 = 0; i2 < N; i2 += NS) {
-          for (int i1 = 0; i1 < N; i1 += NS) {
-            for (int i0 = 0; i0 < N; i0 += NS) {
+        for (int i2 = 0; i2 < M_N; i2 += M_NS) {
+          for (int i1 = 0; i1 < M_N; i1 += M_NS) {
+            for (int i0 = 0; i0 < M_N; i0 += M_NS) {
+#if (M_MEM == MEM_DATATYPE)
               // get the datatype corresponding to the sub-array
               MPI_Datatype type1, type2;
-              MPI_Type_create_hvector(NS, NS, N * sizeof(int), MPI_INT, &type1);
-              MPI_Type_create_hvector(NS, 1, N * N * sizeof(int), type1,
+              MPI_Type_create_hvector(M_NS, M_NS, M_N * sizeof(int), MPI_INT, &type1);
+              MPI_Type_create_hvector(M_NS, 1, M_N * M_N * sizeof(int), type1,
                                       &type2);
               MPI_Type_commit(&type2);
               MPI_Type_free(&type1);
+              int offset = i0 + M_N * (i1 + M_N * i2);
+#elif (M_MEM == MEM_CONTIG)
+              MPI_Datatype type2;
+              MPI_Type_create_hvector(1, M_NS * M_NS * M_NS, sizeof(int),
+                                      MPI_INT, &type2);
+              MPI_Type_commit(&type2);
+              int offset = (M_NS * M_NS * M_NS) * ireq;
+#endif
 
-              int offset = i0 + N * (i1 + N * i2);
 #if (M_COMM == COMM_RMA_ACTV || M_COMM == COMM_RMA_FENC)
               // do the MPI_Get
               MPI_Get(other + offset, 1, type2, next_rank, offset, 1, type2,
@@ -120,8 +144,8 @@ int main(int argc, char** argv){
                         rreq + ireq);
               MPI_Isend(array + offset, 1, type2, next_rank, 0, MPI_COMM_WORLD,
                         sreq + ireq);
-              ireq++;
 #endif
+              ireq++;
 
               // free the type
               MPI_Type_free(&type2);
@@ -151,17 +175,17 @@ int main(int argc, char** argv){
         fprintf(stdout,
                 "RMA-ACTIVE: mean send time = %e ms > bandwidth = %e GB/s\n",
                 mtime_global * 1e+3,
-                (N * N * N * sizeof(double)) / mtime_global * 1e+9);
+                (M_N * M_N * M_N * sizeof(int)) / mtime_global / 1e+9);
 #elif (M_COMM == COMM_RMA_FENC)
         fprintf(stdout,
                 "RMA-FENCE: mean send time = %e ms > bandwidth = %e GB/s\n",
                 mtime_global * 1e+3,
-                (N * N * N * sizeof(double)) / mtime_global * 1e+9);
+                (M_N * M_N * M_N * sizeof(int)) / mtime_global / 1e+9);
 #elif (M_COMM == COMM_SENDRECV)
         fprintf(stdout,
                 "SEND-RECV: mean send time = %e ms > bandwidth = %e GB/s\n",
                 mtime_global * 1e+3,
-                (N * N * N * sizeof(double)) / mtime_global * 1e+9);
+                (M_N * M_N * M_N * sizeof(int)) / mtime_global / 1e+9);
 #endif
         fflush(stdout);
     }
